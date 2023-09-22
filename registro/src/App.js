8aeddca5b2adb5645091ae6a6c16f5c7a6785c9b
DorisMauVer <doris.maureira.vera@gmail.com>
2023-09-22 20:14:24 -0300

    Creación de registro usuario
    se crea el formulario de registro de usuario mediante estructura de node js con tailwind y react

---
+++ registro/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import RegisForm from './RegisForm';
+
+function App() {
+  return (
+    
+    <div className="App">
+    <header className="App-header">
+      <RegisForm />
+    </header>
+  </div>
+  );
+}
+
+export default App;
