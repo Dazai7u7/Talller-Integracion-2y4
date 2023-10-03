60bb685e2ac23340e40fe9766d6bcc0bd41d1264
Demian Binimelis <dbinimelis2022@alu.uct.cl>
2023-10-03 16:35:32 -0300

    se añadio css con tailwind al login

--- Node-JS/client/src/pages/paginaLogin.jsx
+++ Node-JS/client/src/pages/paginaLogin.jsx
@@ -16,38 +16,46 @@ function PaginaLogin() {
     });
 
     return (
-        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-            <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
-                {LoginErrors.map((error, i) => (
-                    <div className="bg-red-500 p-2 text-white text-center" key={i}>
-                        { error }
-                    </div>
-                ))}
+        <div class=" bg-slate-200">
 
-                <h1 className="text-2xl font-bold">Login</h1>
+            <div className="flex h-[calc(100vh-100px)] items-center justify-center">
 
-                <form onSubmit={onSubmit}>
-                    <input type="email" {...register('email', {required: true})}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="Correo"
-                    />
-                    {errors.email && <p className="text-red-500">Un email es requerido</p>}
-                    
-                    <input type="password" {...register('password', {required: true})}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="Contraseña"
-                    />
-                    {errors.password && (
-                        <p className="text-red-500">Una contraseña es requerida</p>
-                    )}
+                <div className="bg-white max-w-md w-full p-10 rounded-md shadow-lg">
+                    {LoginErrors.map((error, i) => (
+                        <div className="bg-red-500 p-2 text-white text-center" key={i}>
+                            { error }
+                        </div>
+                    ))}
 
-                    <button type="submit">Ingresar</button>
-                </form>
-                
-                <p className="flex gap-x-2 justify-between">
-                    ¿No estas registrado aun? <Link to="/registro" className="text-sky-500">Registrate</Link>
-                </p>
-            </div> 
+                    <h1 className="text-2xl font-bold text-black text-center">Login</h1>
+                    <hr className="mt-2 bg-black shadow"></hr>
+                    <form onSubmit={onSubmit}>
+                        <p class="text-black mt-4 fond-bold">Correo</p>
+                        <input type="email" {...register('email', {required: true})}
+                            className="w-full bg-white border border-black text-black px-4 py-2 rounded-md my-2 focus:outline-teal-400"
+
+                        />
+                        {errors.email && <p className="text-red-500">Un email es requerido</p>}
+                        <p className="text-black mt-2">Contraseña: </p>
+                        <input type="password" {...register('password', {required: true})}
+                            className="w-full bg-white border border-black text-black px-4 py-2 rounded-md my-2 focus:outline-teal-400"
+
+                        />
+                        {errors.password && (
+                            <p className="text-red-500">Una contraseña es requerida</p>
+                        )}
+
+                        <button type="submit" className="mt-4 text-white border border-white bg-teal-500 rounded-md w-20 h-8 hover:scale-110 ease-in-out duration-300 hover:text-teal-500 hover:border-teal-500 hover:bg-white">
+                            Ingresar
+                        </button>
+                    </form>
+                        
+                    <p className="flex gap-x-2 justify-between mt-4 text-black">
+                        ¿No estas registrado aun? <Link to="/registro" className="h-7 w-20 text-center border border-white rounded duration-300 text-white bg-teal-500 hover:text-teal-500 hover:border-teal-500 hover:bg-white hover:scale-105">
+                            Registrate</Link>
+                    </p>
+                </div> 
+            </div>
         </div>
     );
 }
