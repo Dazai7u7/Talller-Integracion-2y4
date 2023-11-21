79dc5aabdf6ec4f2253187b496e62473d5c962c4
ryu <rgutierrez2022@alu.uct.cl>
2023-11-21 14:46:31 -0300

    Confirmando cambios locales en auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,9 @@
 import axios from "./axios.js";
+import { useAuth } from '../context/AuthContext';
+
 
 export const registroRequest = usuario => axios.post('/registro', usuario);
 
 export const loginRequest = usuario => axios.post('/login', usuario);
 
-export const verifyTokenRequest = (token) => axios.get('/verify', token);
+export const verifyTokenRequest = (token= useAuth()) => axios.get('/verify', { headers: {"Authorization" : `Bearer ${token}`} });
